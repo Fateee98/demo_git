54e6207bec87bcf203c66183165e18ab8980f4e1
Fate <fate@fates-macbook-pro.local>
2018-08-18 20:30:05 +0700

    Initial Commit

---
+++ demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Fate on 8/18/18.
+//  Copyright © 2018 Fate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
